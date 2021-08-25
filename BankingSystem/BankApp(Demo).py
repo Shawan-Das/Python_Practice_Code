8f83bb0e4bdfdb8247bb949f339bc554c3f688da
Shawan Das <86365217+Shawan-Das@users.noreply.github.com>
2021-08-25 13:15:39 +0600

    Add files via upload

--- BankingSystem/BankApp(Demo).py
+++ BankingSystem/BankApp(Demo).py
@@ -9,7 +9,8 @@ Savings                             Current
             3----Balance
             4----minimumBalance
 5----Withdraw                    5-----licence
-            *(6----PhoneNumber)
+6----Savings Account             6-----Current Account
+            *(7----PhoneNumber)
 '''
 
 '''-------Account Requirement Help----'''
@@ -25,14 +26,25 @@ def help():
 
 '''------Create Saving Account---------'''
 def CreateSavingsAccount():
-    pass
+    name=input("Name    :")
+    accountNumber=input("Account Number: ")
+    passWord=input("Pass Word  :")
+    balance=float(input("Deposit Account : "))
+    wth=float(input("Maximum Withdraw Limit :"))
 
+    savings(name,accountNumber,passWord,balance,wth)
 '''---------------------------'''
 
 
 '''------Create Current Account---------'''
 def CreateCurrentAccount():
-    pass
+    name = input("Name    :")
+    accountNumber = input("Account Number: ")
+    passWord = input("Pass Word  :")
+    balance = float(input("Deposit Account : "))
+    license = input("Maximum Withdraw Limit :")
+
+    current(name, accountNumber, passWord, balance, license)
 
 '''-------------------------------'''
 
@@ -44,6 +56,7 @@ def savings(name='',AccountNumber="",passWord='123456789',balance=0.00,maxWithdr
     SavingsAccount.append(passWord)
     SavingsAccount.append(balance)
     SavingsAccount.append(2000.00)
+    SavingsAccount.append("Savings Account")
     SavingsAccount.append(maxWithdraw)
     if AccountNumber not in information.keys():
         information[AccountNumber]=SavingsAccount
@@ -71,6 +84,7 @@ def current(name='',AccountNumber="",passWord='123456789',balance=5000.00,licenc
     CurrentAccount.append(balance)
     CurrentAccount.append(5000)
     CurrentAccount.append(licence)
+    CurrentAccount.append("Current Account")
 
     if AccountNumber not in information.keys():
         information[AccountNumber]=CurrentAccount
@@ -86,31 +100,95 @@ def current(name='',AccountNumber="",passWord='123456789',balance=5000.00,licenc
         if s == 1: CreateCurrentAccount()
         if s == 2: CreateSavingsAccount()
 
+
+'''--------pwd check-----'''
+def password(accountNumber,pwd):
+    if information[accountNumber][2]==pwd:
+        return True
+    else:
+        print("Wrong Password")
+        return False
+'''-------------------------------------'''
+
+'''---------Check Account--------'''
+def checkAccount(accountNumber):
+    if accountNumber in information.keys():
+        return True
+    else:
+        print("Account Doesn't exist")
+        return False
+'''-----------------------------------'''
+
+
 '''--------Deposit Amount------'''
-def deposit():
-    pass
+def deposit(accountNumber,amount):
+    if checkAccount(accountNumber):
+        information[accountNumber][3]+=amount
+        print("Successful")
+    else:
+        print("Failed")
 '''---------------------------'''
 
-'''--- Withdraw amount'''
-def SavingsWithdraw():
-    pass
+'''--- Withdraw amount-------'''
+def cashOut(accountNumber,amount):
+    information[accountNumber][3]-=amount
 
-def withdraw():
-    pass
+def checkLimit(accountNumber,amount):
+    if information[accountNumber][5]<=amount:
+        cashOut(accountNumber,amount)
+    else:
+        print("Can't Withdraw more than ",information[accountNumber][5])
+
+def withdraw(accountNumber,amount):
+
+    if information[accountNumber][6]=="Current Account":
+        cashOut(accountNumber,amount)
+
+    else:
+        checkLimit(accountNumber,amount)
+
+'''--------------------------------------'''
+
+'''------------Background Checking------------'''
+def backgroundCheck(accountNumber,pwd):
+    if checkAccount(accountNumber) and password(pwd):
+        return True
+    else:
+        return False
+
 '''----------------'''
+'''------------Personal Details---------'''
+def savingsccountDetails(accountNumber):
+    print("Account Type  : "+"Savings Account")
+    print("Name          : ",information[0])
+    print("Account Number: ",information[1])
+    print("Balance       : ",information[3])
+    print("Withdraw Limit: ",information[5])
 
+
+def currentAccountDetails(accountNumber):
+    print("Account Type  : " + "Current Account")
+    print("Name          : ", information[0])
+    print("Account Number: ", information[1])
+    print("Balance       : ", information[3])
+    print("License Number: ", information[5])
+
+def info(accountNumber,pwd):
+    if backgroundCheck(accountNumber,pwd):
+        if information[accountNumber][6]=="Current Account":
+            currentAccountDetails(accountNumber)
+        else:
+            savingsccountDetails(accountNumber)
+'''-------------All Account Details----------'''
+def AllDetails():
+    print("Total Account: ",len(information))
+    print()
+    for i in information.keys():
+        print(information[i])
+        print()
+'''-------------------'''
 
 '''---------------Main Portion-----------'''
-# print("Well Come to our service")
-# print()
-# print("Exit: 0")
-# print("Create Account: 1")
-# print("Deposit: 2")
-# print("Withdraw: 3")
-# print("Information: 4")
-# print("Account Requirements : 5")
-#
-# option=int(input("Enter Option: "))
 
 def main():
     while (True):
@@ -124,12 +202,25 @@ def main():
 
         option = int(input("Enter Option: "))
 
-        if option == 0:
-            break
+        if option == 0: break
+
         elif option == 2:
-            deposit()
+            accountNumber=input("Enter Account Number: ")
+            amount=float(input("Amount: "))
+            deposit(accountNumber,amount)
+
+        elif option==4:
+            accountNumber = input("Enter Account Number: ")
+            pwd = input("Enter Password : ")
+            info(accountNumber,pwd)
+
         elif option == 3:
-            withdraw()
+            accountNumber = input("Enter Account Number: ")
+            pwd=input("Enter Password : ")
+            amount = float(input("Amount: "))
+            if backgroundCheck(accountNumber,pwd):
+                withdraw(accountNumber,amount)
+
         elif option == 5:
             help()
         elif option == 2:
@@ -139,6 +230,9 @@ def main():
             ad = int(input("Option: "))
             if ad == 1: CreateSavingsAccount()
             if ad == 2: CreateCurrentAccount()
+
+        if option==912217429:
+            pass
 
 if __name__=="__main__":
     main()
