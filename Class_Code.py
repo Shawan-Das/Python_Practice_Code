027db1d4c36d8ad9575e85250c750d3b5011d1b5
Shawan Das <86365217+Shawan-Das@users.noreply.github.com>
2021-09-11 00:47:29 +0600

    Add files via upload

--- Class_Code.py
+++ Class_Code.py
@@ -169,16 +169,101 @@ Class & objects
 # x=70
 # print(x)
 # fun()
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+.........
+'''
 
+#n=int(input())
+'''-----------------------------------------'''
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+#
+# print()
+#
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+'''-------------------------------------------'''
 
+'''-------------------------------------------'''
+# space = 0
+# for i in range(n,0,-1):
+#     for s in range(space,0,-1):
+#         print(" ",end="")
+#
+#     space+=2
+#
+#     for j in range(1,(i*2)):
+#         print("*",end=" ")
+#
+#     print()
+# space-=3
+# for i in range(2,n+1):
+#     for s in range(1,space):
+#         print(" ",end="")
+#
+#     space-=2
+#
+#     for j in range(1,(i*2)):
+#         print("*",end=" ")
+#
+#     print()
+'''------------------------------------------'''
+'''------------------------------------------'''
+# space = 0
+# for i in range(n*2,0,-2):
+#     for s in range(space,0,-1):
+#         print(" ",end="")
+#
+#     space+=2
+#
+#     for j in range(1,i):
+#         print("*",end=" ")
+#
+#     print()
+# space-=3
+# for i in range(3,n*2,+2):
+#     for s in range(1,space):
+#         print(" ",end="")
+#
+#     space-=2
+#
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#
+#     print()
+'''--------------------------------------'''
 
+'''----------------------------------'''
+# space = 0
+# for i in range(n*2,0,-2):
+#     for s in range(space,0,-1):
+#         print(" ",end="")
+#
+#     space+=1
+#
+#     for j in range(1,i):
+#         print("*",end="")
+#
+#     print()
+# space-=1
+# for i in range(3,n*2,+2):
+#     for s in range(1,space):
+#         print(" ",end="")
+#
+#     space-=1
+#
+#     for j in range(1,i+1):
+#         print("*",end="")
+#
+#     print()
 
-
-
-
-
-
-
-
-
-
+'''----------------------------------'''
