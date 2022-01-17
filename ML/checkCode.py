e85b9cdddaae7ca4d8f844a90687aa5c8638effd
Shawan Das <shawondas7429@gmail.com>
2022-01-18 01:43:35 +0600

    Binomial Check

--- ML/checkCode.py
+++ ML/checkCode.py
@@ -1,20 +1,13 @@
-import statistics
+import math
+def nCr(x):
+    ans= math.factorial(35)/(math.factorial(x)*math.factorial(35-x))
+    return ans*(0.1**x)*((0.9)**(35-x))
 
-X=[]
-Y=[]
+s=0
+for i in range(11):
+   print(round(nCr(i),4))
+   s+=nCr(i)
 
-for i in range(5):
-    x,y=list(map(int,input().split(" ")))
-    X.append(x)
-    Y.append(y)
-
-sxsy=sum(X)*sum(Y)
-
-sxy=sum(x*y for x,y in zip(X,Y))
-sx2=sum(i**2 for i in X)
-
-b=((5*sxy)-(sxsy))/((5*sx2)-(sum(X)**2))
-a=statistics.mean(Y)-(b*statistics.mean(X))
-
-print(round(a+(b*80),3))
+print(round(s,4))
+print(round(1-s,4))
 
